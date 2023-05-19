70005336e707779bdc1aa71db2929ccac7a13a68
matan <48315169+matanbrimer@users.noreply.github.com>
2023-05-19 17:32:17 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -6,28 +6,27 @@
 #include "sources/smartTeam.hpp"
 #include "sources/TrainedNinja.hpp"
 #include "sources/YoungNinja.hpp"
+#include "sources/Team.hpp"
 
 using namespace ariel;
 
 TEST_CASE("CharacterTest, DefaultConstructor") {
-    Character c;
+    Character c(100,"MATAN",Point(3.0,2.0),"C");
 
-    c.setName("MATAN");
     CHECK_EQ(c.getName(), "MATAN");
     CHECK_EQ(c.isAlive(), true);
 }
 
 TEST_CASE("CharacterTest, HitMethod && isAlive") {
-    Character c;
+    Character c(5,"DANI",Point(3.0,2.0),"C");
     c.hit(6);
     CHECK_EQ(c.isAlive(), false);
 }
 
 TEST_CASE("CharacterTest, DistanceMethod") {
-    Character c1;
-    Character c2;
-    c2.setX(3);
-    c2.setY(4);
+    Character c1(5,"DANI",Point(0.0,0.0),"C");
+    Character c2(5,"DANI",Point(3.0,4.0),"C");
+
     CHECK_EQ(c1.distance(&c2), 5.0);
 }
 
@@ -42,68 +41,72 @@ TEST_CASE("CowboyTest, DefaultConstructor && hasboolets") {
 
 TEST_CASE("CowboyTest, ShootMethod") {
     Cowboy c1("Matan", Point(1,1));
-    Character c2;
+    Character c2(2,"liran",Point(3.0,2.0),"N");
     c1.shoot(&c2);
     CHECK_EQ(c2.isAlive(), false);
 }
 
 TEST_CASE("OldNinjaTest, SlashMethod") {
     OldNinja n("Ninja", Point(2,2));
-    Character c;
-    c.setX(1.5);
-    c.setY(1.5);
+    Character c(2,"liran",Point(1.5,1.5),"N");
+
     n.slash(&c);
     CHECK_EQ(c.isAlive(), false);
 }
 
 TEST_CASE("SmartTeamTest, AddMethod") {
-    Character c;
+    Character c(100,"matan",Point(3.0,2.0),"C");
     Team team(&c);
 
     CHECK_EQ(team.stillAlive(), 1);
+   
 }
 
 TEST_CASE("SmartTeamTest, AttackMethod") {
-    Character c1;
-    Character c2;
+    
+    Character c1(25,"dan",Point(1.0,1.0),"C");
+    Character c2(20,"tom",Point(2.0,2.0),"N");
     smartTeam team1(&c1);
     smartTeam team2(&c2);
-    Character c3;
-    Character c4;
+    Character c3(15,"yoni",Point(5.0,2.0),"C");
+    Character c4(35,"ran",Point(4.0,1.0),"C");
     team1.add(&c3);
     team2.add(&c4);
     team1.attack(&team2);
+    
     CHECK_EQ(team2.stillAlive(), 0);
+    
 }
 
 TEST_CASE("PointTest, SettersAndGetters") {
-    Point p;
-    p.setX(5.0);
-    p.setY(10.0);
+    Point p(5.0,10.0);
+
+   
     CHECK_EQ(p.getX(), 5.0);
     CHECK_EQ(p.getY(), 10.0);
+   
 }
 
 TEST_CASE("PointTest, Distance") {
+      
     Point p1(5.0, 10.0);
     Point p2(10.0, 10.0);
     CHECK_EQ(p1.distance(p2), 5.0);
+    
 }
 
   TEST_CASE("hit") 
     {
-        Point location(0, 0);
-        int life = 100;
-        string name = "DANI";
-        Character test;
+        
+        Character test(100,"ron",Point(33.0,15.0),"N");;
         int damage = 85;
         test.hit(damage);
         CHECK(test.isAlive() == true);
-        
     }
 
     TEST_CASE("shoot , hasboolets , reload")
     {
+        
         Point point1(2, 8);
         Point point2(5, 8);
         Cowboy player1("dani",point1);
@@ -132,4 +135,5 @@ TEST_CASE("PointTest, Distance") {
         CHECK_FALSE(cowboy.hasboolets());
         cowboy.reload();
         CHECK(cowboy.hasboolets());
+        
     }
